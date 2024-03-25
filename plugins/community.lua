a319cf3b03d41398a1c5789c423edbed2715c546
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2024-03-26 01:51:04 +0530

    adds codeium to nvim.

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,7 +1,12 @@
 return {
   -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  { import = "astrocommunity.colorscheme.catppuccin" }
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- { import = "astrocommunity.completion.copilot-lua" }
+  {
+    'Exafunction/codeium.vim',
+    event = 'BufEnter'
+  }
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
